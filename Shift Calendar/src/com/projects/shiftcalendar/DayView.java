bd0c1b6e40eabc4edbe6f98159d8311b22899a3d
mdmo552 <mdmo552@cse.unsw.edu.au>
2012-01-09 09:30:54 +1100

    Separated the dayView class as an XML layout Increased the size of the dayView label text

--- Shift Calendar/src/com/projects/shiftcalendar/DayView.java
+++ Shift Calendar/src/com/projects/shiftcalendar/DayView.java
@@ -2,6 +2,7 @@ package com.projects.shiftcalendar;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -13,55 +14,38 @@ public class DayView extends RelativeLayout {
 	public DayView (Context context) {
 		
 		super (context);
-		this.setup(context);
+		LayoutInflater.from(getContext()).inflate(R.layout.day_view, this, true);
 	}
 	
 	public DayView (Context context, AttributeSet attrs) {
 		
 		super (context, attrs);
-		this.setup(context);
+		LayoutInflater.from(getContext()).inflate(R.layout.day_view, this, true);
 
-	}
-	
-	private void setup (Context context) {
-		
-		this.date = new TextView (context);
-		this.label = new TextView (context);
-		
-		RelativeLayout.LayoutParams dateLayout = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-		dateLayout.addRule(ALIGN_PARENT_TOP);
-		dateLayout.addRule(ALIGN_PARENT_LEFT);
-		
-		RelativeLayout.LayoutParams labelLayout = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-		labelLayout.addRule(ALIGN_PARENT_RIGHT);
-		labelLayout.addRule(ALIGN_PARENT_BOTTOM);
-		
-		this.addView (this.date, dateLayout);
-		this.addView (this.label, labelLayout);
-		
-		this.setBackgroundColor(0xFF999999);
-		
-		this.label.setPadding(0, 0, 16, 8);
 	}
 	
 	public void setDateText (String newtext) {
 		
-		this.date.setText (newtext);
+		TextView t = (TextView) this.findViewById(R.id.day_view_date);
+		t.setText(newtext);
 	}
 	
 	public void setDateColor (int newColor) {
-		
-		this.date.setTextColor(newColor);
+
+		TextView t = (TextView) this.findViewById(R.id.day_view_date);
+		t.setTextColor(newColor);
 	}
 	
 	public void setLabelText (String newtext) {
 		
-		this.label.setText (newtext);
+		TextView t = (TextView) this.findViewById(R.id.day_view_label);
+		t.setText(newtext);
 	}
 	
 	public void setLabelColor (int newColor) {
 		
-		this.label.setTextColor(newColor);
+		TextView t = (TextView) this.findViewById(R.id.day_view_label);
+		t.setTextColor(newColor);
 	}
 	
 	public int getDate () {
