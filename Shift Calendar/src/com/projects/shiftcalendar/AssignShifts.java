6f53aea3644267f1002ca9067c6ec7abc050b83d
mdmo552 <mdmo552@cse.unsw.edu.au>
2012-01-11 15:10:12 +1100

    Removed unneeded imports from AssignShifts.java

--- Shift Calendar/src/com/projects/shiftcalendar/AssignShifts.java
+++ Shift Calendar/src/com/projects/shiftcalendar/AssignShifts.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Debug;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
