81e67a20e4abc5659f39e1076a8aaf6db35d570f
mdmo552 <mdmo552@cse.unsw.edu.au>
2012-01-30 17:14:49 +1100

    Renamed the old ShiftCalender menu into MainMenu.java to free ShiftCalendar for application context stuff

---
+++ Shift Calendar/src/com/projects/shiftcalendar/MainMenu.java
@@ -0,0 +1,80 @@
+package com.projects.shiftcalendar;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+public class MainMenu extends Activity {
+	
+	private OnClickListener manageShiftsButtonClick = new OnClickListener () {
+
+		public void onClick(View v) {
+			
+			// Change to the manage shifts activity
+			Intent i = new Intent ();
+			ComponentName actName = new ComponentName("com.projects.shiftcalendar", "com.projects.shiftcalendar.ManageShifts");
+			i.setComponent(actName);
+			startActivity (i);
+		}
+	};
+	
+	private OnClickListener assignShiftsButtonClick = new OnClickListener () {
+
+		public void onClick(View v) {
+			
+			// Change to the manage shifts activity
+			Intent i = new Intent ();
+			ComponentName actName = new ComponentName("com.projects.shiftcalendar", "com.projects.shiftcalendar.AssignShifts");
+			i.setComponent(actName);
+			startActivity (i);
+		}
+	};
+	
+	private OnClickListener viewMonthButtonClick = new OnClickListener () {
+
+		public void onClick(View v) {
+			
+			// Change to the manage shifts activity
+			Intent i = new Intent ();
+			ComponentName actName = new ComponentName("com.projects.shiftcalendar", "com.projects.shiftcalendar.ViewMonth");
+			i.setComponent(actName);
+			startActivity (i);
+		}
+	};
+	
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home);
+		
+		// Grab the three buttons
+		
+		View manageShiftsButton = findViewById(R.id.home_manage_shifts);
+		manageShiftsButton.setOnClickListener(manageShiftsButtonClick);
+		
+		View assignShiftsButton = findViewById(R.id.home_assign_shifts);
+		assignShiftsButton.setOnClickListener(assignShiftsButtonClick);
+		
+		View viewMonthButton = findViewById(R.id.home_view_month);
+		viewMonthButton.setOnClickListener(viewMonthButtonClick);
+		
+		// Check for first run
+		SharedPreferences settings = getPreferences(MODE_PRIVATE);
+		if (settings.getBoolean("firstRun", true)) {
+			
+			SharedPreferences.Editor editor = settings.edit();
+			editor.putBoolean("firstRun", false);
+			editor.commit();
+			
+			Toast.makeText(getApplicationContext(), R.string.first_run_help_toast, Toast.LENGTH_LONG).show();
+		}
+	}
+}
